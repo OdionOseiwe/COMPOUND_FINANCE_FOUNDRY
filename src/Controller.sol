2da2cbfa62fc75406649c61dec447ea69d66b763
OdionOseiwe <oseiweifebhor@gmail.com>
2022-12-14 19:37:52 +0100

    ...

--- src/Controller.sol
+++ src/Controller.sol
@@ -17,10 +17,14 @@ contract Controller is ReentrancyGuard, Ownable{
 
     mapping(address => bool) markets;
 
+    address[] public s_allowedTokens;
+
     // Account -> Token -> Amount
     mapping(address => mapping(address => uint256)) public m_accountToTokenDeposits;
     // Account -> Token -> Amount
     mapping(address => mapping(address => uint256)) public m_accountToTokenBorrows;
+
+
 
     // 5% Liquidation Reward
     uint256 public constant LIQUIDATION_REWARD = 500;
@@ -39,6 +43,17 @@ contract Controller is ReentrancyGuard, Ownable{
     ///////////////////////////////////////////////////////EVENTS//////////////////////////////////////////////////
     event Deposit(address depositor, uint amount, address token);
     event Borrow(address debtor, address tokenAddress, uint256 amount);
+    event Repay(address payer, uint256 amount);
+    event Withdraw(address indexed account, address indexed token, uint256 indexed amount);
+    event Repay(address indexed account, address indexed token, uint256 indexed amount);
+    event Liquidate(
+        address indexed account,
+        address indexed repayToken,
+        address indexed rewardToken,
+        uint256 halfDebtInEth,
+        address liquidator
+    );
+    error TransferFailed();
 
     /////////////////////////////////////////////////////////MODIFIER/////////////////////////////////////////////
     
@@ -56,36 +71,133 @@ contract Controller is ReentrancyGuard, Ownable{
         if(markets[token]){
             m_accountToTokenDeposits[msg.sender][token] += amount;
             emit Deposit(msg.sender,amount,token);
-            bool sent = IERC20(token).transferFrom(msg.sender, address(token), amount);
+            bool sent = IERC20(token).transferFrom(msg.sender, address(this), amount);
             require(sent, "failed to sent into contract");
         }
     }
 
-    function borrow(uint256 amount, address tokenBorrow, address tokenCollateral) external  zeroAddress(tokenCollateral) zeroAddress(tokenBorrow) zeroAmount(amount) nonReentrant{
-        if(markets[tokenBorrow]){
-            require(IERC20(tokenBorrow).balanceOf(address(this)) >= amount, "Not enough tokens to borrow");
-            m_accountToTokenBorrows[msg.sender][tokenBorrow] += amount;
-            emit Borrow(msg.sender, tokenBorrow, amount);
-            bool sent = IERC20(tokenBorrow).transfer(msg.sender, amount);
-            require(sent, "failed to send");
-            uint256 allow = allowed(msg.sender,tokenBorrow,tokenCollateral);
-            require(allow > COLLATERALFACTORMIN, "contract will not be balanced");
+    function borrow(address token, uint256 amount)
+        external
+        nonReentrant
+       zeroAddress(token) zeroAmount(amount)
+    {
+        if(markets[token]){
+            require(IERC20(token).balanceOf(address(this)) >= amount, "Not enough tokens to borrow");
+            m_accountToTokenBorrows[msg.sender][token] += amount;
+            emit Borrow(msg.sender, token, amount);
+            bool success = IERC20(token).transfer(msg.sender, amount);
+            if (!success) revert TransferFailed();
+            require(healthFactor(msg.sender) >= COLLATERALFACTORMIN, "Platform will go insolvent!");
         }
     }
 
-    function allowed(address borrower, address tokenBorrow, address tokenCollateral) view private returns(uint256){
-        uint256 collateral = m_accountToTokenDeposits[borrower][tokenCollateral];
-        uint maximumBorrow  =  (collateral * LIQUIDATION_THRESHOLD) / 10000;
-        uint maxBorrowUSD = getUSDValue(tokenCollateral,maximumBorrow );
-        uint usersBorrows = m_accountToTokenBorrows[borrower][tokenBorrow];
-        uint userBorrowsUSD = getUSDValue(tokenBorrow,usersBorrows );
-        return (maxBorrowUSD * 1e18) / userBorrowsUSD;
+   function withdraw(address token, uint256 amount) external nonReentrant zeroAddress(token) zeroAmount(amount) {
+        require(m_accountToTokenDeposits[msg.sender][token] >= amount, "Not enough funds");
+        emit Withdraw(msg.sender, token, amount);
+        _pullFunds(msg.sender, token, amount);
+        require(healthFactor(msg.sender) >= COLLATERALFACTORMIN, "Platform will go insolvent!");
     }
 
-    function getUSDValue(address token, uint256 amount) private view returns (uint256) {
+    function _pullFunds(
+        address account,
+        address token,
+        uint256 amount
+    ) private {
+        require(m_accountToTokenDeposits[account][token] >= amount, "Not enough funds to withdraw");
+        m_accountToTokenDeposits[account][token] -= amount;
+        bool success = IERC20(token).transfer(msg.sender, amount);
+        if (!success) revert TransferFailed();
+    }
+
+    function liquidate(
+        address account,
+        address repayToken,
+        address rewardToken
+    ) external nonReentrant {
+        require(healthFactor(account) < COLLATERALFACTORMIN, "Account can't be liquidated!");
+        uint256 halfDebt = m_accountToTokenBorrows[account][repayToken] / 2;
+        uint256 halfDebtInUSD = getUSDValue(repayToken, halfDebt);
+        require(halfDebtInUSD > 0, "Choose a different repayToken!");
+        uint256 rewardAmountInUSD = (halfDebtInUSD * LIQUIDATION_REWARD) / 100;
+        uint256 totalRewardAmountInRewardToken = getTokenValueFromUSD(
+            rewardToken,
+            rewardAmountInUSD + halfDebtInUSD
+        );
+        emit Liquidate(account, repayToken, rewardToken, halfDebtInUSD, msg.sender);
+        _repay(account, repayToken, halfDebt);
+        _pullFunds(account, rewardToken, totalRewardAmountInRewardToken);
+    }
+
+    function repay(address token, uint256 amount)
+        external
+        nonReentrant
+        zeroAddress(token) zeroAmount(amount)
+    {
+        emit Repay(msg.sender, token, amount);
+        _repay(msg.sender, token, amount);
+    }
+
+    function _repay(
+        address account,
+        address token,
+        uint256 amount
+    ) private {
+        // require(m_accountToTokenBorrows[account][token] - amount >= 0, "Repayed too much!");
+        // On 0.8+ of solidity, it auto reverts math that would drop below 0 for a uint256
+        m_accountToTokenBorrows[account][token] -= amount;
+        bool success = IERC20(token).transferFrom(msg.sender, address(this), amount);
+        if (!success) revert TransferFailed();
+    }
+
+    function getAccountInformation(address user)
+        public
+        view
+        returns (uint256 borrowedValueInUSD, uint256 collateralValueInUSD)
+    {
+        borrowedValueInUSD = getAccountBorrowedValue(user);
+        collateralValueInUSD = getAccountCollateralValue(user);
+    }
+
+    function getAccountCollateralValue(address user) public view returns (uint256) {
+        uint256 totalCollateralValueInUSD = 0;
+        for (uint256 index = 0; index < s_allowedTokens.length; index++) {
+            address token = s_allowedTokens[index];
+            uint256 amount = m_accountToTokenDeposits[user][token];
+            uint256 valueInUSD = getUSDValue(token, amount);
+            totalCollateralValueInUSD += valueInUSD;
+        }
+        return totalCollateralValueInUSD;
+    }
+
+    function getAccountBorrowedValue(address user) public view returns (uint256) {
+        uint256 totalBorrowsValueInUSD = 0;
+        for (uint256 index = 0; index < s_allowedTokens.length; index++) {
+            address token = s_allowedTokens[index];
+            uint256 amount = m_accountToTokenBorrows[user][token];
+            uint256 valueInUSD = getUSDValue(token, amount);
+            totalBorrowsValueInUSD += valueInUSD;
+        }
+        return totalBorrowsValueInUSD;
+    }
+
+    function getUSDValue(address token, uint256 amount) public view returns (uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(s_tokenToPriceFeed[token]);
         (, int256 price, , , ) = priceFeed.latestRoundData();
         return (uint256(price) * amount) / 1e18;
+    }
+
+    function getTokenValueFromUSD(address token, uint256 amount) public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_tokenToPriceFeed[token]);
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return (amount * 1e18) / uint256(price);
+    }
+
+    function healthFactor(address account) public view returns (uint256) {
+        (uint256 borrowedValueInUSD, uint256 collateralValueInUSD) = getAccountInformation(account);
+        uint256 collateralAdjustedForThreshold = (collateralValueInUSD * LIQUIDATION_THRESHOLD) /
+            100;
+        if (borrowedValueInUSD == 0) return 100e8;
+        return (collateralAdjustedForThreshold * 1e8) / borrowedValueInUSD;
     }
 
 }
